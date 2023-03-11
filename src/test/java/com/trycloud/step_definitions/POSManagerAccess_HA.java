4a53ee807c8d6cfe5ca26396fdd695734f33a37b
hakan <hakanaydinus@hotmail.com>
2023-03-10 20:54:13 -0500

    POSManagerAccess_HA step definition file has been created

---
+++ src/test/java/com/trycloud/step_definitions/POSManagerAccess_HA.java
@@ -0,0 +1,39 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class POSManagerAccess_HA {
+    @Given("POS manager is on the log in page")
+    public void pos_manager_is_on_the_log_in_page() {
+
+    }
+
+    @When("the POS manager enters username {string}")
+    public void the_pos_manager_enters_username(String string) {
+
+    }
+
+    @When("the POS manager enter password {string}")
+    public void the_pos_manager_enter_password(String string) {
+
+    }
+
+    @When("the POS manager clicks the log in button")
+    public void the_pos_manager_clicks_the_log_in_button() {
+
+    }
+
+    @Then("account holder's name should be {string}")
+    public void account_holder_s_name_should_be(String string) {
+
+    }
+
+
+
+
+
+
+
+}
